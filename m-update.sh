afe39e2feeec69899c9283039a0c2f488e491878
User058 <110759720+User058@users.noreply.github.com>
2025-01-10 10:23:45 +0300

    Update m-update.sh

--- m-update.sh
+++ m-update.sh
@@ -119,32 +119,32 @@ fun_bar() {
 }
 res1() {
     
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/User058/vip/main/updt/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/m-trgo "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-trgo.sh" && chmod +x /usr/bin/m-trgo
-wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/User058/vip/main/updt/menu/restore.sh" && chmod +x /usr/bin/restore
-wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/User058/vip/main/updt/menu/backup.sh" && chmod +x /usr/bin/backup
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/User058/vip/main/updt/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/m-trgo "https://raw.githubusercontent.com/User058/vip/main/updt/m-trgo.sh" && chmod +x /usr/bin/m-trgo
+wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/User058/vip/main/updt/restore.sh" && chmod +x /usr/bin/restore
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/User058/vip/main/updt/backup.sh" && chmod +x /usr/bin/backup
 wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/User058/vip/main/updt/bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/User058/vip/main/updt/bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
-wget -q -O /usr/bin/m-noobz "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-noobz.sh" && chmod +x /usr/bin/m-noobz
-wget -q -O /usr/bin/m-ip "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-ip.sh" && chmod +x /usr/bin/m-ip
-wget -q -O /usr/bin/m-bot "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-bot.sh" && chmod +x /usr/bin/m-bot
-wget -q -O /usr/bin/m-theme "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-theme.sh" && chmod +x /usr/bin/m-theme
-wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-vmess.sh" && chmod +x /usr/bin/m-vmess
-wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-vless.sh" && chmod +x /usr/bin/m-vless
-wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-trojan.sh" && chmod +x /usr/bin/m-trojan
-wget -q -O /usr/bin/m-system "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-system.sh" && chmod +x /usr/bin/m-system
-wget -q -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/User058/vip/main/updt/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/m-backup "https://raw.githubusercontent.com/User058/vip/main/updt/menu/m-backup.sh" && chmod +x /usr/bin/m-backup
+wget -q -O /usr/bin/m-noobz "https://raw.githubusercontent.com/User058/vip/main/updt/m-noobz.sh" && chmod +x /usr/bin/m-noobz
+wget -q -O /usr/bin/m-ip "https://raw.githubusercontent.com/User058/vip/main/updt/m-ip.sh" && chmod +x /usr/bin/m-ip
+wget -q -O /usr/bin/m-bot "https://raw.githubusercontent.com/User058/vip/main/updt/m-bot.sh" && chmod +x /usr/bin/m-bot
+wget -q -O /usr/bin/m-theme "https://raw.githubusercontent.com/User058/vip/main/updt/m-theme.sh" && chmod +x /usr/bin/m-theme
+wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/User058/vip/main/updt/m-vmess.sh" && chmod +x /usr/bin/m-vmess
+wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/User058/vip/main/updt/m-vless.sh" && chmod +x /usr/bin/m-vless
+wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/User058/vip/main/updt/m-trojan.sh" && chmod +x /usr/bin/m-trojan
+wget -q -O /usr/bin/m-system "https://raw.githubusercontent.com/User058/vip/main/updt/m-system.sh" && chmod +x /usr/bin/m-system
+wget -q -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/User058/vip/main/updt/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/User058/vip/main/updt/running.sh" && chmod +x /usr/bin/running
+wget -q -O /usr/bin/m-backup "https://raw.githubusercontent.com/User058/vip/main/updt/m-backup.sh" && chmod +x /usr/bin/m-backup
 wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/User058/vip/main/updt/speedtest_cli.py" && chmod +x /usr/bin/speedtest
-wget -q -O /usr/bin/bckpbot "https://raw.githubusercontent.com/User058/vip/main/updt/menu/bckpbot.sh" && chmod +x /usr/bin/bckpbot
-wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/User058/vip/main/updt/menu/tendang.sh" && chmod +x /usr/bin/tendang
-wget -q -O /usr/bin/bottelegram "https://raw.githubusercontent.com/User058/vip/main/updt/menu/bottelegram.sh" && chmod +x /usr/bin/bottelegram
-wget -q -O /usr/bin/xraylimit "https://raw.githubusercontent.com/User058/vip/main/updt/menu/xraylimit.sh" && chmod +x /usr/bin/xraylimit
-wget -q -O /usr/bin/trialvmess "https://raw.githubusercontent.com/User058/vip/main/updt/menu/trialvmess.sh" && chmod +x /usr/bin/trialvmess
-wget -q -O /usr/bin/trialvless "https://raw.githubusercontent.com/User058/vip/main/updt/menu/trialtrojan.sh" && chmod +x /usr/bin/trialtrojan
-wget -q -O /usr/bin/trialtrojan "https://raw.githubusercontent.com/User058/vip/main/updt/menu/trialvless.sh" && chmod +x /usr/bin/trialvless
-wget -q -O /usr/bin/trialssh "https://raw.githubusercontent.com/User058/vip/main/updt/menu/trialssh.sh" && chmod +x /usr/bin/trialssh
+wget -q -O /usr/bin/bckpbot "https://raw.githubusercontent.com/User058/vip/main/updt/bckpbot.sh" && chmod +x /usr/bin/bckpbot
+wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/User058/vip/main/updt/tendang.sh" && chmod +x /usr/bin/tendang
+wget -q -O /usr/bin/bottelegram "https://raw.githubusercontent.com/User058/vip/main/updt/bottelegram.sh" && chmod +x /usr/bin/bottelegram
+wget -q -O /usr/bin/xraylimit "https://raw.githubusercontent.com/User058/vip/main/updt/xraylimit.sh" && chmod +x /usr/bin/xraylimit
+wget -q -O /usr/bin/trialvmess "https://raw.githubusercontent.com/User058/vip/main/updt/trialvmess.sh" && chmod +x /usr/bin/trialvmess
+wget -q -O /usr/bin/trialvless "https://raw.githubusercontent.com/User058/vip/main/updt/trialtrojan.sh" && chmod +x /usr/bin/trialtrojan
+wget -q -O /usr/bin/trialtrojan "https://raw.githubusercontent.com/User058/vip/main/updt/trialvless.sh" && chmod +x /usr/bin/trialvless
+wget -q -O /usr/bin/trialssh "https://raw.githubusercontent.com/User058/vip/main/updt/trialssh.sh" && chmod +x /usr/bin/trialssh
 wget -q -O /usr/bin/autocpu "https://raw.githubusercontent.com/User058/vip/main/updt/install/autocpu.sh" && chmod +x /usr/bin/autocpu
 wget -q -O /usr/bin/bantwidth "https://raw.githubusercontent.com/User058/vip/main/updt/install/bantwidth" && chmod +x /usr/bin/bantwidth
 chmod +x menu
